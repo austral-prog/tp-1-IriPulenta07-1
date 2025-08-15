d058b215013ccd39a7d8892a425939726b838075
IriPulenta07 <ipulenta@mail.austral.edu.ar>
2025-08-15 15:58:06 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    c = a + b
+    print(c)
+    d = a - b
+    print(d)
+    e = a * b
+    print(e)
+    f = (a + b)/2
+    print(f)
+    g = a//b
+    print(g)
+    h = a%b
+    print(h)
+    i = a/b
+    print(i)
